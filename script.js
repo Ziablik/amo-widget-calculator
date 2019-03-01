f3dfb9e919dc63a9a2ed42b0937dc5eda1121956
Alex <alexdev@alarmcrm.ru>
2019-03-01 13:55:39 +0300

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-'use strict'
-var FreePackWidgetEnv = 'prod'
-if (FreePackWidgetEnv === 'dev') {
-  define(
-    ['https://localhost:8080/free-pack/src/index.js?v=' + Math.random()],
-    function (widget) {
-      return widget
-    })
-} else {
-  define(['./src/index.js'], function (widget) {
-    return widget
-  })
-}
+'use strict';
+// var FreePackWidgetEnv = 'prod';
+// if (FreePackWidgetEnv === 'dev') {
+//   define(
+//     ['https://localhost:8080/amo-widget-calculator/src/index.js?v=' + Math.random()],
+//     function (widget) {
+//       return widget
+//     })
+// } else {
+//   define(['./src/index.js'], function (widget) {
+//     return widget
+//   })
+// }
+define(['https://localhost:8080/amo-widget-calculator/src/index.js?v='+Math.random()], function (widget) {
+    return widget;
+});
+
+
--- src/index.js
+++ src/index.js
@@ -1,65 +1,367 @@
 define(['jquery'], function ($) {
-
-  /**
-   * @returns {Widget}
-   * @constructor
-   */
-  var Widget = function () {
-    var self = this
-    var wcode, settings, users, wurl, enabled;
-
     /**
-     * Функция для загрузки шаблонов по из папки templates
-     * @param template
-     * @param params
-     * @param callback
-     * @returns {*|boolean}
+     *
+     * @returns {Widget}
+     * @constructor
      */
-    self.getTemplate = function (template, params, callback) {
-      params = (typeof params === 'object') ? params : {}
-      template = template || ''
+    var Widget = function () {
+        var self = this;
+        system = self.system;
+        var wcode, settings, users, wurl, enabled;
 
-      return self.render({
-        href:  '/templates/' + template + '.twig',
-        base_path: wurl, //тут обращение к объекту виджет вернет /widgets/#WIDGET_NAME#
-        load: callback, //вызов функции обратного вызова
-      }, params) //параметры для шаблона
-    }
+        /**
+         * Функция для загрузки шаблонов по из папки templates
+         * @param template
+         * @param params
+         * @param callback
+         * @returns {*|boolean}
+         */
+        self.getTemplate = function (template, params, callback) {
+            params = (typeof params === 'object') ? params : {};
+            template = template || '';
 
-    /**
-     * @type {{render: (function(): boolean), init: (function(): boolean), bind_actions: (function(): boolean), settings: (function(): boolean), onSave: (function(): boolean), destroy: PandoraLoaderWidget.callbacks.destroy}}
-     */
-    this.callbacks = {
-      render: function () {
-        wcode = settings.widget_code
+            return self.render({
+                href:  '/templates/' + template + '.twig',
+                base_path: wurl, //тут обращение к объекту виджет вернет /widgets/#WIDGET_NAME#
+                load: callback, //вызов функции обратного вызова
+            }, params) //параметры для шаблона
+        };
 
-        // Подгружаем стили
-        if (FreePackWidgetEnv === 'dev') {
-          wurl = 'https://localhost:8080/free-pack'
-        } else {
-          wurl = '/upl/' + wcode + '/widget'
-        }
+        this.get_settings = function () {
+            var tp;
+            $.ajax({
+                type: 'POST',
+                cache: false,
+                dataType: 'json',
+                async: false,
+                url: 'https://new5c608a588697c.amocrm.ru/ajax/widgets/list',
+                data: '',
+                success: function (data) {
+                    tp=JSON.parse(data.widgets.lastochka.settings);
+                }
+            });
+            return tp;
+        };
 
-        return true
-      },
-      init: function () {
-        return true
-      },
-      bind_actions: function () {
-        return true
-      },
-      settings: function () {
-        return true
-      },
-      onSave: function () {
-        return true
-      },
-      destroy: function () {
-        return true
-      }
-    }
-    return this
-  }
+        this.set_settings = function (codeField, formul) {
+            var data = self.get_settings();
+            data[codeField] = formul;
 
-  return Widget
-})
+            $.post('https://new5c608a588697c.amocrm.ru/ajax/widgets/edit', {
+                action: 'edit',
+                id: '348835',
+                code: 'lastochka',
+                widget_active: 'Y',
+                settings: data
+            }, function () {
+                console.log(self.get_settings());
+            });
+        };
+
+        this.delete_settings = function (codeFiled) {
+            var data = self.get_settings();
+            delete data[codeFiled];
+            $.post('https://new5c608a588697c.amocrm.ru/ajax/widgets/edit', {
+                action: 'edit',
+                id: '348835',
+                code: 'lastochka',
+                widget_active: 'Y',
+                settings: data
+            }, function () {
+                console.log(self.get_settings());
+            });
+        };
+
+        this.getFieldsNames = function () {
+            var fieldsNames = [];
+            fieldsNames.push({option: 'Бюджет', id: 'sale'});
+            //Получаю все имена и id всех кастомных полей, а также поля бюджет, и заношу их в объект fieldsNames
+            //Берет только поля типа число
+            $.get('https://new5c608a588697c.amocrm.ru/api/v2/account?with=custom_fields', function (data) {
+                var leads = data._embedded.custom_fields.leads;
+                for (key in leads){
+                    if(leads[key].field_type == 2){
+                        fieldsNames.push({option: leads[key].name, id: leads[key].id});
+                    }
+                }
+                //Выбор кастомного поля, чтобы создать для него формулу
+                $('#work-area-lastochka').append(self.render(
+                    {ref: '/tmpl/controls/select.twig'},// объект data в данном случае содержит только ссылку на шаблон
+                    {
+                        items: fieldsNames,
+                        id: 'mainField',   //указание id
+                    })
+                );
+                $('.control--select--button ').css('width', '15%');
+                $('.control--select').append('=');
+
+                //Поле для ввода формулы
+                $('.control--select').append(self.render(
+                    {ref: '/tmpl/controls/input.twig'},// объект data в данном случае содержит только ссылку на шаблон
+                    {
+                        id: 'formulField'   //указание id
+                    }));
+                $('#formulField').css('width', '50%');
+
+                //Кнопка для сохранения формулы
+                $('.control--select').append(self.render(
+                    {ref: '/tmpl/controls/button.twig'},// объект data в данном случае содержит только ссылку на шаблон
+                    {
+                        id: 'buttonSaveFormul'   //указание id
+                    }));
+                $('#buttonSaveFormul span').append('Создать формулу');
+                $('#buttonSaveFormul').css('background', '#4c8bf7').css('color', '#fff');
+
+                $('#buttonSaveFormul').on('click', function () {
+                    if(self.validateFormul($('#formulField').val(), fieldsNames, $('.control--select--button-inner').text())){
+                        alert('Формула создана');
+                        $('#formulField').css('border', '1px solid rgb(0,255,0)');
+                        self.set_settings($('.control--select--button span').text(), $('#formulField').val())
+                    }
+                    else{
+                        alert("Не правильное оформление");
+                        $('#formulField').css('border', '1px solid rgb(255,0,0)')
+                    }
+                })
+            });
+        };
+
+        //Функция отрисовывает все существующие формулы на странице расширненных настроек
+        this.getFormuls = function () {
+            var formuls = self.get_settings();
+            for(key in formuls){
+                if(key != 'login'){
+                    $('#work-area-lastochka').append('<div>' +
+                        '<a href="" class="spoiler_links">Спойлер</a>' +
+                        '<div class="control" id="formul-info">'+ key +'='+ formuls[key] +'</div>' +
+                        '</div>');
+                }
+            }
+            $("div#formul-info").hide('normal');
+            $('.spoiler_links').on('click', function(){
+                $("div#formul-info").hide('normal');
+                $(this).parent().children('div#formul-info').toggle('normal');
+                return false;
+            });
+            $('div#formul-info').each(function () {
+                $(this).append(self.render(
+                    {ref: '/tmpl/controls/button.twig'},// объект data в данном случае содержит только ссылку на шаблон
+                    {
+                        id: 'buttonDeleteFormul'   //указание id
+                    }));
+                $(this).find('button').css('background', 'rgb(240,0,0)').css('color', '#fff');
+                $(this).find('span').append('Удалить формулу');
+                var mainField = $(this).html(),
+                    codeField = '';
+                for(i=0; i<mainField.length; i++){
+                    if(mainField[i] != '='){
+                        codeField = codeField + mainField[i]
+                    }
+                    if(mainField[i] == '='){
+                        break;
+                    }
+                }
+                $(this).parent().find('a').text(codeField);
+                $(this).find('button').on('click', function () {
+                    if(confirm('Вы уверены, что хотите удалить формулу?')){
+                        self.delete_settings(codeField);
+                        $(this).parent().parent().detach();
+                        alert('Формула удалена');
+                    }
+                })
+            })
+        };
+
+        //Расчет по формуле
+        this.calculation = function (arrFormuls) {
+            for(i = 0; i<arrFormuls.length; i++){
+                // console.log(arrFormuls[i]);
+                var formul = '';
+                for(j=1; j<arrFormuls[i].length; j++){
+                    formul = formul + arrFormuls[i][j];
+                }
+                try {
+                    $('[name="CFV['+ arrFormuls[i][0] +']"]').val(eval(formul));
+                } catch (err) {
+                    console.log('Заполните поля, учавствующие в формуле')
+                }
+            }
+        };
+
+        //Формирование формулы
+        //Замена имен полей в формуле на id полей а затем замена на value поля по id
+        this.convertFormul = function () {
+            var formuls = self.get_settings(),
+                formul,
+                arrFormuls = [],
+                arrFieldsID = [];
+            delete formuls['login'];
+            for(formul in formuls){
+                var arrFormul = self.parseFormul(formuls[formul]);
+                for(i=0; i<arrFormul.length; i++){
+                    if(arrFormul[i].length > 1){
+                        arrFormul[i] = self.convertFieldName(arrFormul[i]);
+                        arrFieldsID.push(arrFormul[i]);
+                        if(arrFormul[i] == 'lead_card_budget'){
+                            arrFormul[i] = $('#'+arrFormul[i]).val().replace(/\s/g, '');
+                        }
+                        else{
+                            arrFormul[i] = $('[name="CFV['+ arrFormul[i] +']"]').val();
+                        }
+                    }
+                }
+                arrFormul.unshift(self.convertFieldName(formul));
+                arrFormuls.push(arrFormul);
+            }
+            self.fieldsAction(arrFieldsID);
+            return arrFormuls;
+        };
+
+        //Создает обработчик для полей, которые используются в формуле
+        this.fieldsAction = function (arrFieldsID) {
+            for(i=0; i<arrFieldsID.length; i++){
+                if(arrFieldsID[i] == 'lead_card_budget'){
+                    $('#'+arrFieldsID[i]).on('keyup', function () {
+                        self.calculation(self.convertFormul());
+                    })
+                }
+                else{
+                    $('[name="CFV['+ arrFieldsID[i] +']"]').on('keyup', function () {
+                        self.calculation(self.convertFormul());
+                    })
+                }
+            }
+        };
+
+        //Конвертировать имя поля на id поля
+        this.convertFieldName = function (fieldName) {
+            // console.log(fieldName);
+            var fields,
+                field;
+            $.ajax({
+                type:'GET',
+                async: false,
+                url: 'https://new5c608a588697c.amocrm.ru/api/v2/account?with=custom_fields',
+                success: function (data) {
+                    fields = data._embedded.custom_fields.leads;
+                }
+            });
+            for(field in fields){
+                if(fieldName == 'Бюджет'){
+                    return 'lead_card_budget'
+                }
+                if(fieldName == fields[field].name){
+                    return field;
+                }
+            }
+        };
+
+        //Валидация формулы, mainField не должен повторяться в тексте формулы, все поля используемые в формуле должны существовать
+        this.validateFormul = function (formul, fieldsNames, mainField) {
+            // console.log(formul, fieldsNames, mainField);
+            var arrFormul = self.parseFormul(formul);
+            if(!arrFormul){
+                return false
+            }
+            for(i=0; i<arrFormul.length; i++){
+                if(arrFormul[i].length > 1){
+                    if(arrFormul[i] == mainField){
+                        return false
+                    }
+                    for(j=0; j<fieldsNames.length; j++){
+                        if(arrFormul[i] == fieldsNames[j].option){
+                            break;
+                        }
+                        else if(j == fieldsNames.length-1){
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        };
+
+        //parse текста формулы в массив, первый элемент может быть либо "(", либо имя поля,
+        //bracketCount на выходе должен быть равен 0 и не должен становиться <0 во время процесса
+        this.parseFormul = function (formul) {
+            var bracketCount = 0,
+                arrFormul = [],
+                arrSymbols = '()+-*/0123456789',
+                arrField = '';
+            for(i=0; i<formul.length; i++){
+                if(arrSymbols.indexOf(formul[i]) != -1){
+                    arrField = '';
+                    if(arrSymbols.indexOf(formul[i]) == 0){
+                        bracketCount++;
+                    }
+                    else if(arrSymbols.indexOf(formul[i]) == 1){
+                        bracketCount--;
+                    }
+                    if(bracketCount < 0){
+                        return false;
+                    }
+                    arrFormul.push(formul[i]);
+                }
+                else{
+                    arrField = arrField + formul[i];
+                    if(arrSymbols.indexOf(formul[i+1]) != -1 || i == formul.length-1){
+                        arrFormul.push(arrField);
+                    }
+                }
+            }
+            if((arrSymbols.indexOf(arrFormul[0]) == 0 || arrFormul[0].length > 1) && bracketCount == 0){
+                return arrFormul;
+            }
+            return false;
+        };
+
+        /**
+         * @type {{render: (function(): boolean), init: (function(): boolean), bind_actions: (function(): boolean), settings: (function(): boolean), onSave: (function(): boolean), destroy: PandoraLoaderWidget.callbacks.destroy}}
+         */
+        this.callbacks = {
+            render: function () {
+                console.log('render');
+                // wcode = settings.widget_code;
+                //
+                // //Подгружаем стили
+                // if (FreePackWidgetEnv === 'dev') {
+                //     wurl = 'https://localhost:8080/amo-widget-calculator'
+                // } else {
+                //     wurl = '/upl/' + wcode + '/widget'
+                // }
+
+                return true
+            },
+            init: function () {
+                console.log('init');
+                return true
+            },
+            bind_actions: function () {
+                console.log('bind_action');
+                if(self.system().area == 'lcard'){
+                    self.calculation(self.convertFormul());
+                }
+                return true;
+            },
+            advancedSettings: function() {
+                console.log('advanced');
+                console.log('Test');
+                self.getFieldsNames();
+                self.getFormuls();
+
+                return true;
+            },
+            settings: function () {
+                return true
+            },
+            onSave: function () {
+                return true
+            },
+            destroy: function () {
+                return true
+            }
+        };
+        return this
+    };
+    return Widget
+});
