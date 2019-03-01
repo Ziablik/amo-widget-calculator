144bcea1c386663417e393588b26948ece50600b
Alex <alexdev@alarmcrm.ru>
2019-03-01 17:33:02 +0300

    upd

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 'use strict';
-// var FreePackWidgetEnv = 'prod';
-// if (FreePackWidgetEnv === 'dev') {
-//   define(
-//     ['https://localhost:8080/amo-widget-calculator/src/index.js?v=' + Math.random()],
-//     function (widget) {
-//       return widget
-//     })
-// } else {
-//   define(['./src/index.js'], function (widget) {
-//     return widget
-//   })
-// }
-define(['https://localhost:8080/amo-widget-calculator/src/index.js?v='+Math.random()], function (widget) {
-    return widget;
-});
-
+var FreePackWidgetEnv = 'dev';
+if (FreePackWidgetEnv === 'dev') {
+  define(
+    ['https://localhost:8080/amo-widget-calculator/src/index.js?v=' + Math.random()],
+    function (widget) {
+      return widget
+    })
+} else {
+  define(['./src/index.js'], function (widget) {
+    return widget
+  })
+}
 
